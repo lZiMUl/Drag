4b718a2b70f2cc18850a2577e08859bdf2496c09
lZiMUl <AA2908554069@gmail.com>
2022-09-22 12:28:07 +0800

    fix

--- drag.js
+++ drag.js
@@ -1,76 +1,76 @@
 class Drag {
-  
+
   static browserWidth = document.documentElement.clientWidth || document.body.clientWidth;
   static browserHeight = document.documentElement.clientHeight || document.body.clientHeightclientHeight;
   static tagsName = ['div', 'span'];
-  
+
   constructor(item) {
     Drag.reset(Drag.tagsName);
-    
+
     if (Drag.tagsName.find((_item) => _item.toLocaleUpperCase() === item.nodeName)) {
       item.setAttribute('style', 'position: absolute;');
       item.addEventListener('touchstart', Drag.touchStart.bind(item));
-      };
+    };
   };
-  
+
   static touchStart({ touches }) {
     const [{
       clientX,
       clientY,
-    },{
+    }, {
       offsetLeft,
       offsetTop
     }] = [
-      touches[0],
-      this
-    ];
-    
+        touches[0],
+        this
+      ];
+
     this.disX = clientX - offsetLeft;
     this.disY = clientY - offsetTop;
-    
+
     this.addEventListener('touchmove', Drag.touchMove.bind(this));
   };
-  
+
   static touchMove({ touches }) {
     const [{
       clientX,
       clientY,
-        }, {
+    }, {
       offsetWidth,
       offsetHeight
-        }] = [
-          touches[0],
-          this
-        ];
-    
+    }] = [
+        touches[0],
+        this
+      ];
+
     let [
       X,
       Y
     ] = [
-      clientX - this.disX,
-      clientY - this.disY
-    ];
-    
+        clientX - this.disX,
+        clientY - this.disY
+      ];
+
     if (X >= Drag.browserWidth - offsetWidth) {
       X = Drag.browserWidth - offsetWidth;
     };
-    
+
     if (X < 0) {
       X = 0;
     };
-    
+
     if (Y >= Drag.browserHeight - offsetHeight) {
       Y = Drag.browserHeight - offsetHeight;
     };
-    
+
     if (Y < 0) {
       Y = 0;
     };
-    
+
     this.style.left = X + 'px';
     this.style.top = Y + 'px';
   };
-  
+
   static reset(arr) {
     Drag.tagsName = Array.from(new Set(arr));
   };
